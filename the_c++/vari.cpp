ba771be1ebd349890f35f1dd344dc9f07c8b01b3
shanmukavenkat <21bq1a4208@vvit.net>
2026-01-06 22:58:40 +0530

    just_datatypes

---
+++ the_c++/vari.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    /* variable declarations */ 
+    bool is_true = true ;
+    string course = "c++";
+    int score = 989;
+    float rating = 12.3456;
+    cout << is_true << " it is boolean expression"<<endl ;
+    cout << course << " it is the string data type" << endl;
+    cout << score << " it is the integer data type" << endl;
+    cout << rating << " it is the float data type " << endl;
+
+    return 0;
+}
