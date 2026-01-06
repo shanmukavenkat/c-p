0ce191a2074e25ffaa23e7ff5ac075d43f34e0fd
shanmukavenkat <21bq1a4208@vvit.net>
2026-01-06 23:08:58 +0530

    revising

---
+++ the_c++/primi.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    cout<< " the primitive data types are like the short, int , long long, float, double,bool,char" << endl;
+    cout << "the short small integer 16(2 bytes) ==> short s = 32000" << endl;
+    cout << "the int is medium integer 32(4 bytes) ==? int i = 123456" << endl;
+    cout << "the long long is the large integer 64(8 bytes) => long long ll = 12345678912345" << endl;
+    cout << "the float is the single precision 32(4bytes) ==> float f = 123.4f" << endl;
+    return 0;
+}
