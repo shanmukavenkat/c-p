aac7cd84efb6685690690b8ee91b055e7cc81995
shanmukavenkat <21bq1a4208@vvit.net>
2026-01-06 22:38:51 +0530

    c++

---
+++ the_c++/operators.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    bool x = true;
+    bool y = false;
+    cout<< x && y ;
+    cout << x || y;
+    cout << x;
+    return 0;
+}
---
+++ the_c++/part-2.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    string s;
+    getline(cin,s);
+    cout << s;
+    return 0;
+}
+
+/*
+primitive and non primitive 
+
+the collection of characters is called the  string  but the string is the primitive 
+and the character is the primitive
+
+*/
